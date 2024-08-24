0171d8a8c59b3ec8e97770c6bcbc663dbd35cad6
rxsi1920 <preranapinkoppa192@gmail.com>
2024-08-24 21:12:58 +1000

    update

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = ""
+app = Flask(__name__, template_folder="src/templates")
+app.config['SECRET_KEY'] = "f7G@k8!r^V2jL&x9*ZbQ0$uP#nT1mW"
 app.config['APPLICATION_ROOT'] = '/src'
 
 @app.route('/')
 def index():
-    return render_template('./index.html')
+    return render_template('index.html')
+
+@app.route('/user')
+def user():
+    return render_template()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    resp = make_response(render_template('404.html'), 404)
+    return resp
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="3000")
+    app.run(host="0.0.0.0", port=3000)
